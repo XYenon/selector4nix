95a7fef228c64e0e857335546bf4cb0a464baae8
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2026-05-06 21:13:37 +0800

    refactor(selector4nix): split NarUseCase

--- src/api/state.rs
+++ src/api/state.rs
@@ -2,7 +2,7 @@ use std::sync::Arc;
 
 use getset::Getters;
 
-use crate::application::nar::NarUseCase;
+use crate::application::nar::usecase::{NarResolutionUseCase, NarStreamingUseCase};
 use crate::application::substituter::usecase::SubstituterQueryUseCase;
 use crate::infrastructure::config::CacheInfoConfiguration;
 
@@ -10,19 +10,22 @@ use crate::infrastructure::config::CacheInfoConfiguration;
 #[getset(get = "pub")]
 pub struct AppContext {
     substituter_query_usecase: SubstituterQueryUseCase,
-    nar_usecase: NarUseCase,
+    nar_resolution_usecase: NarResolutionUseCase,
+    nar_streaming_usecase: NarStreamingUseCase,
     cache_info: CacheInfoConfiguration,
 }
 
 impl AppContext {
     pub fn new(
         substituter_query_usecase: SubstituterQueryUseCase,
-        nar_usecase: NarUseCase,
+        nar_resolution_usecase: NarResolutionUseCase,
+        nar_streaming_usecase: NarStreamingUseCase,
         cache_info: CacheInfoConfiguration,
     ) -> Arc<Self> {
         Arc::new(Self {
             substituter_query_usecase,
-            nar_usecase,
+            nar_resolution_usecase,
+            nar_streaming_usecase,
             cache_info,
         })
     }
