b63755220d111e83d7ef7d2c780e872a68de49e6
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2026-05-24 21:15:00 +0800

    chore(nix): add nix-serve-ng to dev environment

--- nix/flake/devshell.nix
+++ nix/flake/devshell.nix
@@ -10,6 +10,7 @@
       devShells.default = pkgs.mkShellNoCC {
         packages = [
           config.packages.rust-toolchain
+          pkgs.nix-serve-ng
           pkgs.nixfmt
           pkgs.nixfmt-tree
         ];
