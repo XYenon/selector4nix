90af6c8be12c16a98b72aae37aa587bb1894f157
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2026-05-26 21:02:48 +0800

    refactor(selector4nix): remove dead code in `Substituter` aggregate

--- src/domain/substituter/model/availability.rs
+++ src/domain/substituter/model/availability.rs
@@ -60,29 +60,6 @@ impl Availability {
         }
     }
 
-    pub fn update_and_check_availability(self, now: Instant) -> (bool, Self) {
-        match &self {
-            Self::Offline { detected_at } => {
-                if *detected_at + Self::OFFLINE_RETRY_PERIOD <= now {
-                    (true, self.try_change_to_maybe_ready())
-                } else {
-                    (false, self)
-                }
-            }
-            Self::ServiceError {
-                detected_at,
-                prev_failures,
-            } => {
-                if *detected_at + Self::calc_retry_duration(*prev_failures) <= now {
-                    (true, self.try_change_to_maybe_ready())
-                } else {
-                    (false, self)
-                }
-            }
-            _ => (true, self),
-        }
-    }
-
     pub fn retry_duration(&self) -> Option<Duration> {
         match self {
             Self::Offline { .. } => Some(Self::OFFLINE_RETRY_PERIOD),
@@ -142,56 +119,5 @@ mod tests {
                 prev_failures: 1,
             }
         );
-    }
-
-    #[test]
-    fn update_and_check_availability_returns_true_given_normal() {
-        let (available, new_state) =
-            Availability::Normal.update_and_check_availability(Instant::now());
-        assert!(available);
-        assert_eq!(new_state, Availability::Normal);
-    }
-
-    #[test]
-    fn update_and_check_availability_returns_false_when_service_error_before_timeout() {
-        let now = Instant::now();
-        let state = Availability::ServiceError {
-            detected_at: now,
-            prev_failures: 0,
-        };
-        let (available, _) = state.update_and_check_availability(now);
-        assert!(!available);
-    }
-
-    #[test]
-    fn update_and_check_availability_returns_true_when_service_error_after_timeout() {
-        let now = Instant::now();
-        let state = Availability::ServiceError {
-            detected_at: now,
-            prev_failures: 0,
-        };
-        let retry_at = now + Duration::from_millis(500);
-        let (available, new_state) = state.update_and_check_availability(retry_at);
-        assert!(available);
-        assert_eq!(new_state, Availability::MaybeReady { prev_failures: 0 });
-    }
-
-    #[test]
-    fn update_and_check_availability_returns_true_when_offline_before_timeout() {
-        let now = Instant::now();
-        let state = Availability::Offline { detected_at: now };
-        let retry_at = now + Availability::OFFLINE_RETRY_PERIOD - Duration::from_millis(1);
-        let (available, _) = state.update_and_check_availability(retry_at);
-        assert!(!available);
-    }
-
-    #[test]
-    fn update_and_check_availability_returns_true_when_offline_after_timeout() {
-        let now = Instant::now();
-        let state = Availability::Offline { detected_at: now };
-        let retry_at = now + Availability::OFFLINE_RETRY_PERIOD + Duration::from_millis(1);
-        let (available, new_state) = state.update_and_check_availability(retry_at);
-        assert!(available);
-        assert_eq!(new_state, Availability::MaybeReady { prev_failures: 0 });
     }
 }
